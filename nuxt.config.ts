5157bd33f9cf26f82404e12cf677169cfd33fd0f
milyasbpa <basilyas.2015@gmail.com>
2025-02-15 19:21:19 +0700

    config: initialization

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap",
+        },
+      ],
+    },
+  },
+  devServer: {
+    port: process.env.NUXT_PORT ? Number(process.env.NUXT_PORT) : 3000,
+  },
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-[blue] font-bold">welcome index</h1>
+  </div>
+</template>
